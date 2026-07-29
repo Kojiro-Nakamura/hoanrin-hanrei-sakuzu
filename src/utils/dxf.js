2bfbd14f8934cd918db44832f5a169b9651c28da
Kojiro-Nakamura <Kojiro-Nakamura@users.noreply.github.com>
2026-07-29 22:28:34 +0900

    Apply global EXPORT_SCALE (100/60) to DXF geometry to correct scale issue

--- src/utils/dxf.js
+++ src/utils/dxf.js
@@ -1,23 +1,42 @@
+const EXPORT_SCALE = 100 / 60;
+
 export const dxfCreateText = (text, x, y, height, layer="0", color=7) => {
+  x *= EXPORT_SCALE; y *= EXPORT_SCALE; height *= EXPORT_SCALE;
   const escapedText = String(text).split('').map(c => {
     const code = c.charCodeAt(0);
     return code > 127 ? '\\U+' + code.toString(16).toUpperCase().padStart(4, '0') : c;
   }).join('');
   return `  0\r\nTEXT\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${x.toFixed(4)}\r\n 20\r\n${(-y).toFixed(4)}\r\n 30\r\n0.0\r\n 40\r\n${height.toFixed(4)}\r\n  1\r\n${escapedText}\r\n 72\r\n1\r\n 11\r\n${x.toFixed(4)}\r\n 21\r\n${(-y).toFixed(4)}\r\n 31\r\n0.0\r\n 73\r\n2\r\n`; 
 };
-export const dxfCreateCircle = (cx, cy, r, layer="0", color=7) => `  0\r\nCIRCLE\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${cx.toFixed(4)}\r\n 20\r\n${(-cy).toFixed(4)}\r\n 30\r\n0.0\r\n 40\r\n${r.toFixed(4)}\r\n`;
-export const dxfCreateInsert = (blockName, cx, cy, scale, angleDeg, layer="0", color=7) => `  0\r\nINSERT\r\n  2\r\n${blockName}\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${cx.toFixed(4)}\r\n 20\r\n${(-cy).toFixed(4)}\r\n 30\r\n0.0\r\n 41\r\n${scale.toFixed(4)}\r\n 42\r\n${scale.toFixed(4)}\r\n 43\r\n1.0\r\n 50\r\n${(-angleDeg).toFixed(4)}\r\n`;
-export const dxfCreateSolid = (x1, y1, x2, y2, x3, y3, x4, y4, layer="0", color=7) => `  0\r\nSOLID\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${x1.toFixed(4)}\r\n 20\r\n${(-y1).toFixed(4)}\r\n 30\r\n0.0\r\n 11\r\n${x2.toFixed(4)}\r\n 21\r\n${(-y2).toFixed(4)}\r\n 31\r\n0.0\r\n 12\r\n${x3.toFixed(4)}\r\n 22\r\n${(-y3).toFixed(4)}\r\n 32\r\n0.0\r\n 13\r\n${x4.toFixed(4)}\r\n 23\r\n${(-y4).toFixed(4)}\r\n 33\r\n0.0\r\n`;
+
+export const dxfCreateCircle = (cx, cy, r, layer="0", color=7) => {
+  cx *= EXPORT_SCALE; cy *= EXPORT_SCALE; r *= EXPORT_SCALE;
+  return `  0\r\nCIRCLE\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${cx.toFixed(4)}\r\n 20\r\n${(-cy).toFixed(4)}\r\n 30\r\n0.0\r\n 40\r\n${r.toFixed(4)}\r\n`;
+};
+
+export const dxfCreateInsert = (blockName, cx, cy, scale, angleDeg, layer="0", color=7) => {
+  cx *= EXPORT_SCALE; cy *= EXPORT_SCALE; scale *= EXPORT_SCALE;
+  return `  0\r\nINSERT\r\n  2\r\n${blockName}\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${cx.toFixed(4)}\r\n 20\r\n${(-cy).toFixed(4)}\r\n 30\r\n0.0\r\n 41\r\n${scale.toFixed(4)}\r\n 42\r\n${scale.toFixed(4)}\r\n 43\r\n1.0\r\n 50\r\n${(-angleDeg).toFixed(4)}\r\n`;
+};
+
+export const dxfCreateSolid = (x1, y1, x2, y2, x3, y3, x4, y4, layer="0", color=7) => {
+  x1 *= EXPORT_SCALE; y1 *= EXPORT_SCALE;
+  x2 *= EXPORT_SCALE; y2 *= EXPORT_SCALE;
+  x3 *= EXPORT_SCALE; y3 *= EXPORT_SCALE;
+  x4 *= EXPORT_SCALE; y4 *= EXPORT_SCALE;
+  return `  0\r\nSOLID\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${x1.toFixed(4)}\r\n 20\r\n${(-y1).toFixed(4)}\r\n 30\r\n0.0\r\n 11\r\n${x2.toFixed(4)}\r\n 21\r\n${(-y2).toFixed(4)}\r\n 31\r\n0.0\r\n 12\r\n${x3.toFixed(4)}\r\n 22\r\n${(-y3).toFixed(4)}\r\n 32\r\n0.0\r\n 13\r\n${x4.toFixed(4)}\r\n 23\r\n${(-y4).toFixed(4)}\r\n 33\r\n0.0\r\n`;
+};
+
 export const dxfCreateLines = (pts, closed, layer, color) => {
   let res = "";
   if (pts.length < 2) return res;
   for (let i = 0; i < pts.length - 1; i++) {
     const p1 = pts[i], p2 = pts[i+1];
-    res += `  0\r\nLINE\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${p1.x.toFixed(4)}\r\n 20\r\n${(-p1.y).toFixed(4)}\r\n 30\r\n0.0\r\n 11\r\n${p2.x.toFixed(4)}\r\n 21\r\n${(-p2.y).toFixed(4)}\r\n 31\r\n0.0\r\n`;
+    res += `  0\r\nLINE\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${(p1.x * EXPORT_SCALE).toFixed(4)}\r\n 20\r\n${(-p1.y * EXPORT_SCALE).toFixed(4)}\r\n 30\r\n0.0\r\n 11\r\n${(p2.x * EXPORT_SCALE).toFixed(4)}\r\n 21\r\n${(-p2.y * EXPORT_SCALE).toFixed(4)}\r\n 31\r\n0.0\r\n`;
   }
   if (closed) {
     const p1 = pts[pts.length - 1], p2 = pts[0];
-    res += `  0\r\nLINE\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${p1.x.toFixed(4)}\r\n 20\r\n${(-p1.y).toFixed(4)}\r\n 30\r\n0.0\r\n 11\r\n${p2.x.toFixed(4)}\r\n 21\r\n${(-p2.y).toFixed(4)}\r\n 31\r\n0.0\r\n`;
+    res += `  0\r\nLINE\r\n  8\r\n${layer}\r\n 62\r\n${color}\r\n 10\r\n${(p1.x * EXPORT_SCALE).toFixed(4)}\r\n 20\r\n${(-p1.y * EXPORT_SCALE).toFixed(4)}\r\n 30\r\n0.0\r\n 11\r\n${(p2.x * EXPORT_SCALE).toFixed(4)}\r\n 21\r\n${(-p2.y * EXPORT_SCALE).toFixed(4)}\r\n 31\r\n0.0\r\n`;
   }
   return res;
 };
